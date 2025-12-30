dcd40c3f72cb783933832b29b575487cad174c5d
asmahamida <mamidahh@gmail.com>
2025-12-31 00:16:41 +0700

    Baru lagi

---
+++ d32 30 Des 25/test.py
@@ -0,0 +1,30 @@
+print("Hello World!")
+print("Asma")
+if 5 > 2: 
+    print("Five is greater than two!")
+    print("Five is greater than two!")
+
+x = 5
+y = "Hello World"
+print(x)
+print(y)
+#This is just comment
+
+print("Hello"); print("How are you?")
+print('This will also work!')
+
+print("Hello world!", end=" ")
+print("I will print on the same line.")
+print("I am", 5, "years old")
+
+x = str(3)
+y = int(3)
+z = float(3)
+print(x, y, z)
+print(type(x))
+
+#sensitiv terhadap kapital
+a = 4
+A = "Sally"
+print(a)
+print(A)
