cdd284c53f29f1bb75b1aa84e347fdddc02e5a23
asmahamida <mamidahh@gmail.com>
2026-01-03 21:29:39 +0700

    belajar lagi

---
+++ d36 3 Jan 26/belajar.py
@@ -0,0 +1,19 @@
+print(6 & 3) #2
+print(6 | 3) #7 JUMJURRR GANGERTIIIIIIII INI APA BIT BIT
+print(6 ^ 3) #5 OK UDAH NGERTI TP GUNANYA APAA??
+
+# Python Collections (Arrays)
+# There are four collection data types in the Python programming language:
+
+# *List* is a collection which is ordered and changeable. Allows duplicate members.
+# *Tuple* is a collection which is ordered and unchangeable. Allows duplicate members.
+# *Set* is a collection which is unordered, unchangeable*, and unindexed. No duplicate members.
+# *Dictionary* is a collection which is ordered** and changeable. No duplicate members.
+
+thislist = ["apple", "cherry", "banana"]
+print(thislist[-1]) #banana
+print(thislist[2:5])
+
+thislist = ["apple", "banana", "cherry", "orange", "kiwi", "mango"]
+thislist[1:3] = ["blackcurrant", "watermelon"]
+print(thislist)
