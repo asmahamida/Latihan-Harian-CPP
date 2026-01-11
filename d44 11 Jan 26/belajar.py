280e8ef71ae880e5c256cd4dc4fb327afb443120
asmahamida <mamidahh@gmail.com>
2026-01-11 21:26:19 +0700

    belajar lagi

---
+++ d44 11 Jan 26/belajar.py
@@ -0,0 +1,16 @@
+day = 1
+match day:
+    case 1: #combine by | = case 1 | 2 | 3
+        print("Monday")
+    case 2: # add condition = case 2 if month = 5
+        print("Tuesday")
+    case _:
+        print("No Day")
+
+#while and for loops
+#brek, continue, else, sama kaya c++
+
+#create function
+def myFunc():
+    print("Hello!")
+myFunc()
