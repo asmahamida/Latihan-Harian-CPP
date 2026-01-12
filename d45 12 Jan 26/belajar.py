c662e37dfdca638c3f9c726a89f61821306b7d54
asmahamida <mamidahh@gmail.com>
2026-01-12 23:33:42 +0700

    belajar lagi

---
+++ d45 12 Jan 26/belajar.py
@@ -0,0 +1,21 @@
+#FUNCTION
+def my_function(name): # name is a parameter
+  print("Hello", name)
+
+#declaration
+my_function("Emil") # "Emil" is an argument
+
+def my_function(name, /):
+  print("Hello", name)
+my_function("emil")
+
+#*args and **kwargs -> allow func to accept a unknown number of arguments
+#args bisa bikin fungsi dengan parameter tdk ditentukan jumlahnya. args buat tuple
+
+def func(*args):
+  print("type:", type(args))
+  print("first:", args[0])
+  print("scnd:", args[1])
+  print("all:", args)
+
+func("siti", "esti")
