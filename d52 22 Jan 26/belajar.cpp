f9bd84b3875eda2765d258fe33703f737a98313a
asmahamida <mamidahh@gmail.com>
2026-01-22 23:43:48 +0700

    gambar kubus

---
+++ d52 22 Jan 26/belajar.cpp
@@ -0,0 +1,14 @@
+// #lanjut besok
+// #ZIGZAG CONVERSION
+
+// #Input: s = "PAYPALISHIRING", numRows = 3
+// #Output: "PAHNAPLSIIGYIR"
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    string kata;
+    cin >> kata;
+
+}
